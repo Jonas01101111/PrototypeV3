064cea9ae172938488c50b71f0eb467508a5cd23
jonas01101111 <jonas1kiubi@gmail.com>
2025-05-21 11:38:50 -0300

    Adicionar arquivos de projeto.

---
+++ PrototypeV3/Program.cs
@@ -0,0 +1,3 @@
+﻿
+using var game = new PrototypeV3.Game1();
+game.Run();
---
+++ PrototypeV3/Sprite.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrototypeV3
+{
+    internal class Sprite
+    {
+    }
+}
